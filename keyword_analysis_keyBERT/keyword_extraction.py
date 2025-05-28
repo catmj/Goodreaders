a3c143fb9f3ce6c5459698dd220a8b1d92c4f9a1
Yevgeniya Tarasova <ytarasov@umich.edu>
2025-05-28 09:00:29 -0700

    More Playing around

--- keyword_analysis_keyBERT/keyword_extraction.py
+++ keyword_analysis_keyBERT/keyword_extraction.py
@@ -10,14 +10,15 @@ from keybert import KeyBERT
 # IMPORTING COPIES OF DATA
 df1 = pd.read_csv('books_matched_copy.csv')
 df2 = pd.read_csv('reviews_matched_copy.csv')
+df3 = pd.read_csv('preprocessing_books.csv')
 
 kw_model = KeyBERT()
 
 
-def extract(text):
+def extract(text,n=10):
     
     keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words='english',
-                              use_mmr=True, diversity=0.2, nr_candidates=20, top_n=10)
+                              use_mmr=True, diversity=0.2, nr_candidates=20, top_n=n)
 
     keys = []
 
@@ -36,6 +37,11 @@ def extract(text):
 keyword_lists_df1 = df1['description'].astype(str).apply(lambda x:extract(x)) # Create keyword lists.
 #print(keyword_lists_df1)
 keyword_lists_df1.to_csv('output_file_books.csv', index=True) # Convert keyword lists to .csv file.
+
+# KEYWORDS FOR BOOKS.CSV
+keyword_lists_df3 = df3['description'].astype(str).apply(lambda x:extract(x)) # Create keyword lists.
+#print(keyword_lists_df1)
+keyword_lists_df3.to_csv('output_file_preprocessed_books.csv', index=True) # Convert keyword lists to .csv file.
 
 # KEYWORDS FOR REVIEWS.CSV
 keyword_lists_df2 = df2['review'].astype(str).apply(lambda x:extract(x)) # Create keyword lists.

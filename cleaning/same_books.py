4044cc5f798f2ff955d00dd05e4f891bacb2c8f5
KatieJ16 <katie@acornonsite.com>
2025-06-02 11:11:56 -0700

    adding so the large file breaks into chunks

--- cleaning/same_books.py
+++ cleaning/same_books.py
@@ -1,9 +1,26 @@
 import pandas as pd # type: ignore
 import numpy as np  # type: ignore
+import os
 
 
 books = pd.read_csv('../scraping/books.csv')
 reviews = pd.read_csv('../scraping/books_by_user.csv')
+
+
+#if we need to load all files: 
+try:
+    reviews2 = pd.read_csv('../scraping/books_by_user2.csv')
+    reviews23 = pd.read_csv('../scraping/books_by_user23.csv')
+    reviews24 = pd.read_csv('../scraping/books_by_user24.csv')
+    reviews2.columns = ['Title','Author','Rating','User_id','isbn','num_ratings','avg_rating','num_pages',
+                        'review','date_started','date_read','date_added','format','date_pub_edition']
+    reviews23.columns = ['Title','Author','Rating','User_id','isbn','num_ratings','avg_rating','num_pages',
+                        'review','date_started','date_read','date_added','format','date_pub_edition']
+    reviews24.columns = ['Title','Author','Rating','User_id','isbn','num_ratings','avg_rating','num_pages',
+                        'review','date_started','date_read','date_added','format','date_pub_edition']
+    reviews = pd.concat([reviews, reviews2, reviews23, reviews24], ignore_index=True)
+except:
+    pass
 
 books["publication_info"] = pd.to_datetime(books["publication_info"],errors ="coerce")
 
@@ -73,3 +90,38 @@ books_matched.to_csv('../cleaned_data/books_matched.csv')
 reviews_matched.to_csv('../cleaned_data/reviews_matched.csv')
 books_unmatched.to_csv('../cleaned_data/books_unmatched.csv')
 reviews_unmatched.to_csv('../cleaned_data/reviews_unmatched.csv')
+
+
+#save reviews matched into smaller file chunks to fit on github
+# Parameters
+max_size_mb = 95
+max_size_bytes = max_size_mb * 1024 * 1024
+base_filename = '../cleaned_data/reviews_matched'
+
+# Loop to split and save
+start = 0
+file_index = 1
+
+# Guess chunk size (start with a few thousand rows, then adjust)
+chunk_size = 200000  # can tune this later for accuracy
+
+while start < len(reviews_matched):
+    while True:
+        end = min(start + chunk_size, len(reviews_matched))
+        chunk = reviews_matched.iloc[start:end]
+        temp_file = f'{base_filename}_{file_index}.csv'
+        
+        chunk.to_csv(temp_file, index=False)
+
+        # Check file size
+        if os.path.getsize(temp_file) <= max_size_bytes or chunk_size == 1:
+            break
+        else:
+            # Reduce size and try again
+            print("reduce size and try again")
+            print("old size = ", chunk_size, " New size = ", chunk_size * 0.7)
+            chunk_size = int(chunk_size * 0.7)
+
+    print(f'Saved {temp_file} with {len(chunk)} rows')
+    start += len(chunk)
+    file_index += 1

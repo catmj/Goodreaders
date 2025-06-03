a8a59429687a5c4aa7c087c6fe86773ece18558d
[Catherine Johnston] <[catmj1000@gmail.com]>
2025-06-02 20:39:46 -0700

    Ready to scrape, starting now.

--- add_reviews_to_books/add_to_books.py
+++ add_reviews_to_books/add_to_books.py
@@ -15,7 +15,7 @@ HEADERS = {
     "Accept-Language": "en-US, en;q=0.5",
 }
 url_base = "https://www.goodreads.com/book/show/"
-df = pd.read_csv('reviews_unmatched_with_goodreads_urls_filtered.csv')
+df = pd.read_csv('reviews_unmatched_with_goodreads_urls_filtered_no_dupes.csv')
 
 # Function for cleaning strings.
 def clean_string(string):
@@ -39,113 +39,113 @@ def get_genres(soup): # Idea taken from https://stackoverflow.com/questions/7576
 
 # Scraping each book's page.
 for index, row in df.iterrows():
-    if index < 8: # Testing only.
-        url_addendum = df.astype(str).iloc[index,3]
-        start_url = url_base + url_addendum 
-        # book_url_formatted = f"{start_url}"
-        print(start_url)
+    # if index < 8: # Testing only.
+    url_addendum = df.astype(str).iloc[index,3]
+    start_url = url_base + url_addendum 
+    # book_url_formatted = f"{start_url}"
+    # print(start_url)
 
-        res = requests.get(start_url, headers=HEADERS)
-        soup = bs(res.text, 'html.parser')
-        # print(soup)
+    res = requests.get(start_url, headers=HEADERS)
+    soup = bs(res.text, 'html.parser')
+    # print(soup)
 
-        # Missing categories (included in original books.csv dataset).
-        book_votes = "Not applicable"
-        genre = "Not applicable"
-        book_img_url = "Missing"
+    # Missing categories (included in original books.csv dataset).
+    book_votes = "Not applicable"
+    genre = "Not applicable"
+    book_img_url = "Missing"
 
-        # Get easy information.
-        genres = get_genres(soup)
-        book_name = df.astype(str).iloc[index,1]
-        book_author = df.astype(str).iloc[index,2]
-        book_rating = soup.find(class_="RatingStatistics__rating").text.strip()
+    # Get easy information.
+    genres = get_genres(soup)
+    book_name = df.astype(str).iloc[index,1]
+    book_author = df.astype(str).iloc[index,2]
+    book_rating = soup.find(class_="RatingStatistics__rating").text.strip()
 
-        # Get number of book reviews.
-        book_ratings_reviews = soup.find(class_="RatingStatistics__meta").get('aria-label').strip()
-        book_ratings, book_reviews = get_ratings_reviews(book_ratings_reviews)
-        # print(f"Ratings: {book_ratings}, Reviews: {book_reviews}")
+    # Get number of book reviews.
+    book_ratings_reviews = soup.find(class_="RatingStatistics__meta").get('aria-label').strip()
+    book_ratings, book_reviews = get_ratings_reviews(book_ratings_reviews)
+    # print(f"Ratings: {book_ratings}, Reviews: {book_reviews}")
 
-        # Get book blurb.
-        book_description_elements = soup.select('.BookPageMetadataSection__description .Formatted')
-        if book_description_elements:
-            book_description = book_description_elements[0].text
-        else:
-            book_description = 'No description found'
+    # Get book blurb.
+    book_description_elements = soup.select('.BookPageMetadataSection__description .Formatted')
+    if book_description_elements:
+        book_description = book_description_elements[0].text
+    else:
+        book_description = 'No description found'
 
-        # Find number of people wanting to and currently reading.
-        text = soup.get_text()
+    # Find number of people wanting to and currently reading.
+    text = soup.get_text()
 
-        # Use regex to find the number before "people want to read".
-        match = re.search(r'([\d,]+)\s+people want to read', text)
+    # Use regex to find the number before "people want to read".
+    match = re.search(r'([\d,]+)\s+people want to read', text)
 
-        if match:
-            number_str = match.group(1).replace(",", "")
-            want_to_read_num = int(number_str)
-        else:
-            want_to_read_num = -1
+    if match:
+        number_str = match.group(1).replace(",", "")
+        want_to_read_num = int(number_str)
+    else:
+        want_to_read_num = -1
 
-        # Use regex to find the number before "people are currently reading".
-        match = re.search(r'([\d,]+)\s+people are currently reading', text)
-        if match:
-            number_str = match.group(1).replace(",", "")
-            current_read_num = int(number_str)
-        else:
-            current_read_num = -1
+    # Use regex to find the number before "people are currently reading".
+    match = re.search(r'([\d,]+)\s+people are currently reading', text)
+    if match:
+        number_str = match.group(1).replace(",", "")
+        current_read_num = int(number_str)
+    else:
+        current_read_num = -1
 
-        # Add histogram of book ratings.
-        book_ratings_reviews_hist = soup.find_all(class_="RatingsHistogram__labelTotal")#.get('aria-label').strip()
-        hist = []
-        for i in book_ratings_reviews_hist:
-            hist.append(i.text.strip())
+    # Add histogram of book ratings.
+    book_ratings_reviews_hist = soup.find_all(class_="RatingsHistogram__labelTotal")#.get('aria-label').strip()
+    hist = []
+    for i in book_ratings_reviews_hist:
+    hist.append(i.text.strip())
 
-        # Other information.
-        author_avatar_url_element = soup.select('.PageSection .AuthorPreview a.Avatar img.Avatar__image')
-        if author_avatar_url_element:
-            author_avatar_url = author_avatar_url_element[0].get('src')
-        else:
-            author_avatar_url = 'No Avatar found'
-        author_description_element = soup.select('.PageSection > .TruncatedContent .Formatted')
-        if author_description_element:
-            author_description = author_description_element[0].text
-        else:
-            author_description = 'No description found'
-        bookPages = soup.select_one(".FeaturedDetails p[data-testid='pagesFormat']")
-        if bookPages:
-            pages_format = bookPages.text[:bookPages.text.find(" pages")]
-        else:
-            pages_format = "No pages found"
-        publication_info = soup.select_one(".FeaturedDetails p[data-testid='publicationInfo']")
-        if publication_info:
-            publication = publication_info.text[16:]
-        else:
-            publication = "No publication found"
+    # Other information.
+    author_avatar_url_element = soup.select('.PageSection .AuthorPreview a.Avatar img.Avatar__image')
+    if author_avatar_url_element:
+        author_avatar_url = author_avatar_url_element[0].get('src')
+    else:
+        author_avatar_url = 'No Avatar found'
+    author_description_element = soup.select('.PageSection > .TruncatedContent .Formatted')
+    if author_description_element:
+        author_description = author_description_element[0].text
+    else:
+        author_description = 'No description found'
+    bookPages = soup.select_one(".FeaturedDetails p[data-testid='pagesFormat']")
+    if bookPages:
+        pages_format = bookPages.text[:bookPages.text.find(" pages")]
+    else:
+        pages_format = "No pages found"
+    publication_info = soup.select_one(".FeaturedDetails p[data-testid='publicationInfo']")
+    if publication_info:
+        publication = publication_info.text[16:]
+    else:
+        publication = "No publication found"
 
-        book_dict = {
-            "category": genre,
-            "genres": genres,
-            "votes": book_votes,
-            "title": book_name,
-            "description": book_description, 
-            "author_name": book_author,
-            "author_about": author_description, 
-            "avatar_url": author_avatar_url, 
-            "pages": pages_format, 
-            "rating": book_rating, 
-            "num_ratings": book_ratings, 
-            "num_reviews": book_reviews, 
-            "publication_info": publication, 
-            "img_url": book_img_url, 
-            "book_url": start_url,
-            "ratings_hist": hist, 
-            "want_to_read_num": want_to_read_num, 
-            "current_read_num": current_read_num, 
+    book_dict = {
+        "category": genre,
+        "genres": genres,
+        "votes": book_votes,
+        "title": book_name,
+        "description": book_description, 
+        "author_name": book_author,
+        "author_about": author_description, 
+        "avatar_url": author_avatar_url, 
+        "pages": pages_format, 
+        "rating": book_rating, 
+        "num_ratings": book_ratings, 
+        "num_reviews": book_reviews, 
+        "publication_info": publication, 
+        "img_url": book_img_url, 
+        "book_url": start_url,
+        "ratings_hist": hist, 
+        "want_to_read_num": want_to_read_num, 
+        "current_read_num": current_read_num, 
         }
 
-        # for i in book_dict:
-        #     print(i, ":", book_dict[i])
-        # print(book_dict.keys())
+    # for i in book_dict:
+    #     print(i, ":", book_dict[i])
+    # print(book_dict.keys())
 
-        csv_filename = "books_matched_more.csv"
-        with open(csv_filename, mode="a", newline="", encoding="utf-8") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=book_dict.keys(), quoting=csv.QUOTE_ALL)
-            writer.writerow(book_dict)
+    csv_filename = "books_matched_more.csv"
+    with open(csv_filename, mode="a", newline="", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=book_dict.keys(), quoting=csv.QUOTE_ALL)
+        writer.writerow(book_dict)

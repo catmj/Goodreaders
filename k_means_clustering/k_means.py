bf2e9a30c8531c97c705531b96fef0e160104013
[Catherine Johnston] <[catmj1000@gmail.com]>
2025-06-09 12:55:04 -0700

    Minor changes, but need to fix now.

--- k_means_clustering/k_means.py
+++ k_means_clustering/k_means.py
@@ -1,1 +1,6 @@
-import pandas
+# Clustering books by keyword and genre features using k-means.
+import pandas as pd
+
+# Specifying number of clusters and importing feature data.
+num_clusters = 1000
+keyword_df = pd.read_csv('books.csv')
